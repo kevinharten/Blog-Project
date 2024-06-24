a674f8ca611c5d9fbd310bf3ee701d382e2c929e
kevinharten <61008512+kevinharten@users.noreply.github.com>
2024-06-24 01:14:17 +0100

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
-
+const Post = require('../models/Post');
+const User = require('../models/User'); 
 // Routes
 router.get('', async (req, res) => {
     try {
